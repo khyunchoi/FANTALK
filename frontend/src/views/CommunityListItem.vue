37a06564e4af2d0c277d133686cbf5f6615baac6
unknown <paradigmshifter@naver.com>
2022-02-09 08:41:01 +0900

    0209 front community [list, articles, detail]

--- frontend/src/views/CommunityListItem.vue
+++ frontend/src/views/CommunityListItem.vue
@@ -2,30 +2,71 @@
   <div>
 
     <div class="community-list-articles-container">
-      <h1>{{ communinityId }}번 게시판</h1>
+      <h1>{{ communityId }}번 게시판</h1>
+      <h3>커뮤니티 게시글 목록</h3>
       <hr>
-      <h1>커뮤니티 게시글 목록</h1>
 
-      
+      <button
+        v-for="article in articles"
+        style="padding: 3%; border-style: solid; border-width: 0px 0px 3px 0px"
+        @click="enterDetailArticle(article.articleId)"
+      >
+        {{ article.articleId }} | {{ article.title }}
+      </button>
+
     </div>
+
+
+    
 
   </div>
 </template>
 
 <script>
-
-  const communinityId = document.location.pathname.slice(13, 20)
   export default {
     name: 'CommunityListItem',
     components: {
     },
-
     data: function() {
       return {
-        communinityId: communinityId,
-        CommunityListItem: {}
+        communityId: null,
+        communityName: null,
+        articles: {}
       }
     },
+    methods: {
+      // goBack () {
+      //   this.$router.push({ name: 'CommunityList', params: {communityId: this.communityId} })
+      // },
+      enterDetailArticle: function (idx) {
+        this.$router.push({name: 'DetailArticle', params:{ articleId:idx }})
+        this.$router.push({name: 'DetailArticle', params:{ communityId:this.communityId }})
+        DetailArticle.data.push({articleId:idx})
+        DetailArticle.data.push({communityId:this.communityId})
+      }
+    },
+    created: function () {
+      this.communityId = this.$route.params.communityId
+      console.log(this.communityId)
+      this.$axios({
+        method:'get',
+        url: `http://localhost:8080/api/v1/communities/${this.communityId}/articles`
+      })
+      .then(res => {
+        return res.data
+      })
+      .then(res => {
+        this.articles = res
+        console.log(this.articles)
+        return this.articles
+      })
+      .catch(error => {
+          console.log(error)
+      })
+      .finally(function () {
+        console.log('done')
+      })
+    }
   }
 </script>
 
