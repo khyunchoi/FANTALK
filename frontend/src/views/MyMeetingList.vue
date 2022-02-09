5c3e83a3d57ece174c0c1297a4960b8eda8d8912
kimhongjung <hong267@naver.com>
2022-02-09 21:08:23 +0900

    0209 meeting-create [my meeting list page create]

--- frontend/src/views/MyMeetingList.vue
+++ frontend/src/views/MyMeetingList.vue
@@ -1,30 +1,22 @@
 <template>
 	<div>
-    <img src="../assets/sidemymeetinglist.png" alt="check-manager" style="padding: 6% 0 2% 2%;">
-    <div class="container">
-      <h2 style="padding: 0 0 2% 0; font-size: 2.5em;">신청한 팬미팅 목록</h2>
-      <template style="padding: 0.7% 7% 0.7% 7%;">
-        <div>
+    <img src="../assets/sidemymeetinglist.png" alt="mymeetinglist" style="padding: 6% 0 2% 2%;" @click="moveMyMeetingList">
+    <div>
+      <h2 style="padding: 2% 0 2% 16%; font-size: 1.5em;">신청한 팬미팅 목록</h2>
+      <template>
+        <div style="padding: 0.7% 5% 0.7% 10%;">
           <v-data-table
             :headers="headers"
-            :items="desserts"
-            :search="search"
-            hide-actions
-            :pagination.sync="pagination"
+            :items="meetings"
+            :items-per-page="5"
             class="elevation-1"
           >
             <template v-slot:items="props">
-              <td>{{ props.item.name }}</td>
-              <td class="text-xs-right">{{ props.item.calories }}</td>
-              <td class="text-xs-right">{{ props.item.fat }}</td>
-              <td class="text-xs-right">{{ props.item.carbs }}</td>
-              <td class="text-xs-right">{{ props.item.protein }}</td>
-              <td class="text-xs-right">{{ props.item.iron }}</td>
+              <td style="text-align: center;">{{ props.item.title }}</td>
+              <td style="text-align: center;">{{ props.item.openDate }}</td>
+              <td style="text-align: center;">{{ props.item.maxUser }}</td>
             </template>
           </v-data-table>
-          <div class="text-xs-center pt-2">
-            <v-pagination v-model="pagination.page" :length="pages"></v-pagination>
-          </div>
         </div>
       </template>
     </div>
@@ -33,6 +25,7 @@
 
 <script>
   export default {
+
     name: 'MyMeetingList',
      data () {
       return {
@@ -41,40 +34,50 @@
         selected: [],
         headers: [
           {
-            text: 'Dessert (100g serving)',
-            align: 'left',
+            text: '제목',
+            value: 'title',
             sortable: false,
-            value: 'name'
           },
-          { text: 'Calories', value: 'calories' },
-          { text: 'Fat (g)', value: 'fat' },
-          { text: 'Carbs (g)', value: 'carbs' },
-          { text: 'Protein (g)', value: 'protein' },
-          { text: 'Iron (%)', value: 'iron' }
+          { 
+            text: '날짜/시간', 
+            value: 'openDate',
+            sortable: false,
+          },
+          { 
+            text: '신청인원', 
+            value: 'maxUser',
+            sortable: false,
+          },
         ],
-        desserts: [
-          {
-            name: 'Frozen Yogurt',
-            calories: 159,
-            fat: 6.0,
-            carbs: 24,
-            protein: 4.0,
-            iron: '1%'
-          },
-        ]
+        meetings: []
       }
     },
-    computed: {
-      pages () {
-        if (this.pagination.rowsPerPage == null ||
-          this.pagination.totalItems == null
-        ) return 0
-
-        return Math.ceil(this.pagination.totalItems / this.pagination.rowsPerPage)
+    methods : {
+      moveMyMeetingList: function () {
+        this.$router.push({name: 'MyMeetingList'})
+      },
+      moveCreateMeeting: function () {
+        this.$router.push({name: 'CreateMeeting'})
       }
-    }
+    },
+    created: function () {
+      this.$axios({
+        method: 'get',
+        url: `http://localhost:8080/api/v1/meetings/me`,
+      })
+      .then(res => {
+        console.log(res)
+        this.meetings = res.data
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    },
   }
 </script>
 
 <style scoped>
+ img:hover {
+   cursor: pointer;
+ }
 </style>
