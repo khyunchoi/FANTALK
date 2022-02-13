8bdf76574b9d5c69f8614d2670fa0e7741cbd1bd
kimhongjung <hong267@naver.com>
2022-02-13 21:34:22 +0900

    0213 design-meeting [notification update]

--- frontend/src/views/CreateMeeting.vue
+++ frontend/src/views/CreateMeeting.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
-    <img src="../assets/sidemymeetinglist.png" alt="check-manager" style="padding: 6% 0 2% 2%;">
+    <img src="../assets/sidemymeetinglist.png" alt="check-manager" style="padding: 6% 0 2% 2%;" @click="moveMyMeetingList">
     <div style="display: flex; justify-content: center;">
-      <div style="margin: 20px; padding: 20px; width: 700px;">
-        <h3 style="margin-bottom: 20px; padding-bottom: 10px;">
+      <div style="margin: 20px; padding: 20px; width: 70%;">
+        <h2 style="margin-bottom: 20px; padding-bottom: 10px; color: #797BF8;">
           팬미팅 신청
-        </h3>
-        <div style="display: flex; justify-content: center;">
+        </h2>
+        <div style="display: flex; justify-content: center; width: 100%;">
           <v-card
-            style="padding: 20px; border: 1px solid #979797; width: 700px;"
+            style="padding: 20px; border: 1px solid #979797; width: 100%;"
           >
             <CreateMeetingForm />
           </v-card>
@@ -25,5 +25,23 @@
     components: {
       CreateMeetingForm
     },
+    methods : {
+      setToken () {
+        const token = localStorage.getItem('jwt')
+        const config = {
+          Authorization: `Bearer ${token}`
+        }
+        return config
+      },
+      moveMyMeetingList: function () {
+        this.$router.push({name: 'MyMeetingList'})
+      },
+    },
   }
 </script>
+
+<style scoped>
+  img:hover {
+    cursor: pointer
+  }
+</style>
