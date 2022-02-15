105a11c15f089e05e1d6b04c870a0b01bc97cc80
unknown <paradigmshifter@naver.com>
2022-02-15 10:54:50 +0900

    front community-list-item [pagination client]

--- frontend/src/views/CommunityListItem.vue
+++ frontend/src/views/CommunityListItem.vue
@@ -1,12 +1,12 @@
 <template>
   <div style="display:flex; flex-direction: row">
 
-    <div class="community-list-item-side" style="width: 30%;">
+    <div class="community-list-item-side" style="width: 25%;">
       <br><br><br><br>
       <div class="community-list-item-title">
         <h3>{{ communityName }} 팬 커뮤니티</h3>
-        <br>
-        <h5>{{ communityTitle }}</h5>
+        <br><br>
+        <div style="background-color: #797BF8; color: white; width: 70%; text-align: left; padding: 3% 3% 3% 12%; border-radius: 0px 20px 20px 0px;">자유게시판</div>
       </div>
     </div>
 
@@ -26,7 +26,7 @@
         <v-btn
           rounded
           @click="search()"
-          style="background-color: #797BF8; color: white; height: 38px;"
+          style="background-color: gray; color: white; height: 38px;"
         >
           검색
         </v-btn>
@@ -35,17 +35,19 @@
       <br>
       <div class="community-list-item-list">
         <div class="community-list-item-list-articles-1" style="padding-bottom: 5px; border-style: solid; border-width: 0px 0px 2px 0px; border-color: gray;">
-          <span style="width: 10%" class="community-list-item-list-articles-elements">번호</span>
-          <span style="width: 50%" class="community-list-item-list-articles-elements">제목</span>
-          <span style="width: 30%" class="community-list-item-list-articles-elements">작성</span>
           <span style="width: 10%" class="community-list-item-list-articles-elements">조회수</span>
+          <span style="width: 35%" class="community-list-item-list-articles-elements">제목</span>
+          <span style="width: 20%" class="community-list-item-list-articles-elements">작성날짜</span>
+          <span style="width: 35%" class="community-list-item-list-articles-elements">작성자</span>
         </div>
-        <button @click="enterDetailArticle(article.articleId)" class="community-list-item-list-articles-2" v-for="article in articles" :key="article">
-          <span style="width: 10%" class="community-list-item-list-articles-elements">{{ article.articleId }}</span>
-          <span style="width: 50%" class="community-list-item-list-articles-elements">{{ article.title }}</span>
-          <span style="width: 30%" class="community-list-item-list-articles-elements">{{ article.createdAt.slice(0,19) }}</span>
-          <span style="width: 10%" class="community-list-item-list-articles-elements">{{ article.hits }}</span>
-        </button>
+        <div v-for="(article, key) in articles" :key="article.key">
+          <button v-if="key >= articleAtTop && key < articleAtTop + dataPerPage" @click="enterDetailArticle(article.articleId)" class="community-list-item-list-articles-2">
+            <span style="width: 10%" class="community-list-item-list-articles-elements">{{ article.hits }}</span>
+            <span style="width: 35%" class="community-list-item-list-articles-elements">{{ article.title }} (작성 최신순: {{ key }})</span>
+            <span style="width: 20%" class="community-list-item-list-articles-elements">{{ article.createdAt.slice(0,19) }}</span>
+            <span style="width: 35%" class="community-list-item-list-articles-elements">***{{ article.email.slice(3) }}</span>
+          </button>
+        </div>
       </div>
 
       <br>
@@ -60,10 +62,24 @@
         </v-btn>
       </div>
 
+      <div>
+        <div id="pages"></div>
+        <br><br><br><br><br><br>
+        <button @click="paginationMove(1)">[ 1페이지로 가봅시다. ]</button><br>
+        <button @click="paginationMove(2)">[ 2페이지로 가봅시다. ]</button><br>
+        <button @click="paginationMove(3)">[ 3페이지로 가봅시다. ]</button><br>
+        <button @click="paginationMove(4)">[ 4페이지로 가봅시다. ]</button><br>
+        <button @click="paginationMove(5)">[ 5페이지로 가봅시다. ]</button><br>
+        <button @click="paginationMove(6)">[ 6페이지로 가봅시다. ]</button><br>
+        <button @click="paginationMove(7)">[ 7페이지로 가봅시다. ]</button><br>
+        <button @click="paginationMove(8)">[ 8페이지로 가봅시다. ]</button><br>
+        <button @click="paginationMove(9)">[ 9페이지로 가봅시다. ]</button><br>
+      </div>
+
 
     </div>
 
-    <div class="community-list-item-side" style="width: 10%;">
+    <div class="community-list-item-side" style="width: 15%;">
 
     </div>
 
@@ -72,6 +88,8 @@
 
 <script>
   const SERVER_URL = process.env.VUE_APP_API_URL
+  const dataPerPage = 5
+  const pageCount = 3
   export default {
     name: 'CommunityListItem',
     components: {
@@ -104,17 +122,88 @@
             sortable: false,
           },
         ],
+        totalArticleCount: null,
+        dataPerPage: dataPerPage,
+        pageCount: pageCount,
+        totalPage: null,
+
+        articleAtTop: null,
+        // firstPage: null,
+        // lastPage: null,
       }
     },
     methods: {
-      // goBack () {
-      //   this.$router.push({ name: 'CommunityList', params: {communityId: this.communityId} })
-      // },
+
+      paginationSet: function () { // 페이지네이션 구현에 필요한 기본적인 값들을 설정
+        // totalData : 전체 게시글 수
+        // dataPerPage : 한 페이지에 보여지는 게시글 수
+        // pageCount : 한 화면에 보여지는 페이지의 수 (4일 경우 --> [ 1 2 3 4 next ] [ prev 5 6 7 8 next ] [ prev 9 10 11 12 next ] [ prev 13 14 ] 이렇게 최대 4개씩 묶음)
+        // totalPage : 전체 페이지 수
+        const totalData = this.totalArticleCount // totalData : 전체 게시글 수
+        const totalPage = Math.ceil(totalData / dataPerPage) // totalPage : 전체 페이지 수
+        this.totalData = totalData
+        this.totalPage = totalPage
+      },
+
+      paginationChange: function (currentPage) {
+
+        // event.preventDefault()
+
+        const pageGroup = Math.ceil(currentPage / pageCount) // pageGroup : 현재 몇 번째 페이지 묶음에 있는지
+        let last = pageGroup * pageCount // 현재 위치한 페이지 묶음의 마지막 페이지
+        let first = last - (pageCount - 1) // 현재 위치한 페이지 묶음의 첫 번째 페이지
+        if (last > this.totalPage) {last = this.totalPage}
+        if (first < 1) {first = 1}
+        if (this.totalPage <= 1) {first = last}
+        this.articleAtTop = dataPerPage * currentPage - (dataPerPage - 1) // 현재 보여지는 페이지의 제일 상단에 위치한 게시물의 전체 순번
+        const next = last + 1 // 다음 묶음의 첫 번째 페이지
+        const prev = first - 1 // 이전 묶음의 마지막 페이지
+
+        const pages = document.querySelector("#pages")
+        pages.setAttribute("class", "pagination-container")
+
+        if (first > pageCount) {
+          const prevBtn = document.createElement("button")
+          prevBtn.append("<")
+          prevBtn.setAttribute("class", "pagination-btn-prev-next")
+          prevBtn.setAttribute("id", `page(${prev})`)
+          pages.appendChild(prevBtn)
+        }
+        for (let j = first; j <= last; j++) {
+          const pageBtn = document.createElement("button")
+          pageBtn.append((j))
+          if (j === currentPage) {
+            pageBtn.setAttribute("class", "pagination-btn-selected")
+          } else {
+            pageBtn.setAttribute("class", "pagination-btn")
+          }
+          pageBtn.setAttribute("id", `page(${j})`)
+          pages.appendChild(pageBtn)
+        }
+        if (first < Math.ceil(this.totalPage / pageCount) * pageCount - (pageCount - 1)) {
+          const nextBtn = document.createElement("button")
+          nextBtn.setAttribute("class", "pagination-btn-prev-next")
+          nextBtn.setAttribute("id", `page(${next})`)
+          nextBtn.append(">")
+          pages.appendChild(nextBtn)
+        }
+
+      },
+
+      paginationMove: function (pageNum) {
+        const pages = document.querySelector("#pages")
+        pages.replaceChildren()
+        this.paginationChange(pageNum)
+      },
+
       enterDetailArticle: function (idx) {
         console.log('aa')
         this.$router.push({name: 'DetailArticle', params:{ articleId:idx }})
         this.$router.push({name: 'DetailArticle', params:{ communityId:this.communityId }})
+        DetailArticle.data.push({articleId:idx})
+        DetailArticle.data.push({communityId:this.communityId})
       },
+
       enterCreateArticle: function () {
         this.$router.push({name: 'CreateArticle'})
 
@@ -133,11 +222,16 @@
         .catch(err => {
           console.log(err)
         })
-      }
+        .finally(() => {
+          const pagination = document.querySelector("#pages")
+          pagination.remove()
+          
+        })
+      },
     },
     created: function () {
       this.communityId = this.$route.params.communityId
-      console.log(this.communityId)
+      // console.log(this.communityId)
       this.$axios({
         method:'get',
         url: `${SERVER_URL}/api/v1/communities/${this.communityId}/articles`
@@ -148,7 +242,13 @@
       .then(res => {
         this.articles = res
         console.log(this.articles)
-        return this.articles
+        console.log(this.articles.length)
+        this.totalArticleCount = this.articles.length
+        
+        this.paginationSet(1)
+        this.paginationChange(1)
+
+        return
       })
       .catch(error => {
           console.log(error)
@@ -161,7 +261,6 @@
         url: `${SERVER_URL}/api/v1/communities/${this.communityId}`
       })
       .then(res => {
-        console.log(res.data)
         return res.data
       })
       .then(res => {
@@ -228,5 +327,30 @@
     /* background-color: bisque; */
     flex-direction: row-reverse;
     margin: 10px;
+  }
+  .pagination-container {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+  }
+  .pagination-btn {
+    /* background-color: whitesmoke; */
+    border-radius: 5px;
+    padding: 8px;
+    margin: 8px;
+  }
+  .pagination-btn-selected {
+    background-color: #797BF8;
+    color: white;
+    border-radius: 5px;
+    padding: 8px;
+    margin: 8px;
+  }
+  .pagination-btn-prev-next {
+    background-color: lightgray;
+    border-radius: 5px;
+    padding: 8px;
+    margin: 8px;
+  
   }
 </style>
